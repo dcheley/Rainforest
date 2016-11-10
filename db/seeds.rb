3ce7a26ceb7bac891446e9c0b64cd85a68ae4340
Daniel Cheley <danielcheley@hotmail.com>
2016-11-09 21:19:08 -0500

    Generating the products controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+first_product = Product.create(
+  name: "Cup"
+  description: "Made of pure diamond"
+  price_in_cents: 1000
+)
+
+second_product = Product.create(
+  name: "Bowl"
+  description: "Excellent for large meals"
+  price_in_cents: 575
+)
+
+third_product = Product.create(
+  name: "Plate"
+  description: "Our finest piece"
+  price_in_cents: 2800
+)

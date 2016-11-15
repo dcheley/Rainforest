d9dd3d49644037a9b045f533b084d8d487e62318
Daniel Cheley <danielcheley@hotmail.com>
2016-11-15 14:16:19 -0500

    Setting root route to restaurants#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
+
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
